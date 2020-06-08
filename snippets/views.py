3d6922183f4b487936ffd3f23ea2b42c8e589771
gk-svg <gk32239@gmail.com>
2020-06-08 22:15:44 +0530

    changes for authenticate api

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+#Using generic class-based views in rest_framework generic
+
+class SnippetList(generics.ListCreateAPIView):
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly]
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
